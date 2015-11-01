0358e781003ad3b410e5db21b3160a3bb7a0fd3a
Haziman Hashim <haziman@abh.my>
2015-11-01 14:03:42 +0800

    InstallManager

--- lib/Phramework/InstallManager.php
+++ lib/Phramework/InstallManager.php
@@ -2,18 +2,7 @@
 
 class InstallManager {
   var $composer;
-  public static function postInstallCmd(Event $e){
-    $this->composer = $e->getComposer();
-    $vendordir = $this->composer->getConfig()->get('vendor-dir');
-    $zip = new ZipArchive;
-    
-    if ($zip->open(realpath($vendordir.'/jimanx2/phramework/resources/phramework-skeleton.zip')) === TRUE) {
-        $zip->extractTo(realpath($vendordir.'../'));
-        $zip->close();
-        echo 'App skeleton initialized!';
-    } else {
-        echo 'Failed to initialize App skeleton!';
-    }
-    
+  public static function runScript(Event $e){
+  
   }
 }
