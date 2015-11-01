a4683ee9156f96c1f7954eec5f33f74a3233e7ef
Haziman Hashim <haziman@abh.my>
2015-11-01 15:39:34 +0800

    InstallManager

--- lib/Phramework/InstallManager.php
+++ lib/Phramework/InstallManager.php
@@ -4,7 +4,23 @@ use Composer\Script\Event;
 
 class InstallManager {
 
-  public static function runScript(Event $e){
-    die("here");
+  private static function initSkel(Event $event){
+    $zip = new \ZipArchive;
+    $vendorPath = $event->getComposer()->getConfig()->get('vendor-dir');
+    if ($zip->open(realpath($vendorPath.'/jimanx2/phramework/resources/phramework-skeleton.zip')) === TRUE) {
+        $zip->extractTo(realpath($vendorPath.'/../'));
+        $zip->close();
+        echo 'ok';
+    } else {
+        echo 'failed';
+    }
+  }
+  
+  public static function runScript(Event $event){
+    $args = $event->getArguments();
+    switch($args[0]){
+      case "init":
+        return InstallManager::initSkel($event);
+    }
   }
 }
